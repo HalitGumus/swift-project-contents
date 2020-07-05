b6eda4e1184389bcba1604e9971f848463ad07a9
HalitGUMUS <gumushalit47@gmail.com>
2020-07-05 20:06:38 +0300

    [feat] Create main class

---
+++ my-word-archive/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  my-word-archive
+//
+//  Created by HalitG on 5.07.2020.
+//  Copyright © 2020 HalitG. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
