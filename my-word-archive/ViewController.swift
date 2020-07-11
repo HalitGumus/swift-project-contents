b6ae3681d66250db20babb11823efa846152da51
HalitGUMUS <gumushalit47@gmail.com>
2020-07-11 11:04:32 +0300

    [feat] Add firebase auth

--- my-word-archive/ViewController.swift
+++ my-word-archive/ViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
     
@@ -16,7 +17,36 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         view.setGradientBackground(colorTop: Colors.gradientStart, colorBottom: Colors.gradientEnd)
+        
+        
+        signInWithFirebase()
+        
     }
-
+    
+    func signUpWithFirebase(){
+        Auth.auth().createUser(withEmail: "halitgumus47@gmail.com", password: "p12346") { authResult, error in
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            print("Başarılı bir şekilde kayıt yaptınız")
+        }
+    }
+    
+    func signInWithFirebase(){
+        Auth.auth().signIn(withEmail: "halitgumus47@gmail.com", password: "p12346") { [weak self] authResult, error in
+            guard self != nil else { return }
+            
+            if let error = error {
+                print(error)
+                return
+            }
+            
+            print("Başarılı bir şekilde giriş yaptınız")
+        }
+    }
+    
 }
 
